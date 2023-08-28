fe391e3f79738abd853199bd755a05a63e0f3e3d
teoyt <125856128+teoyt@users.noreply.github.com>
2023-08-28 12:08:46 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "database-2.cj4zpjjzzvc2.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "lowchoonkeat-employee1"
+customregion = "us-east-1"
+
