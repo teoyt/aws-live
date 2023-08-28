7f605cb5efb4b40799ace69e8f2d8d11f5993a7b
teoyt <125856128+teoyt@users.noreply.github.com>
2023-08-28 12:11:20 +0800

    Update config.py

--- aws-live-main/config.py
+++ aws-live-main/config.py
@@ -1,7 +1,7 @@
-customhost = "database-1.chauwtk5goof.us-east-1.rds.amazonaws.com"
+customhost = "database-2.cj4zpjjzzvc2.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
 custompass = "Bait3273"
 customdb = "employee"
-custombucket = "lowchoonkeat-employee1"
+custombucket = "teoyutong-bucket"
 customregion = "us-east-1"
 
