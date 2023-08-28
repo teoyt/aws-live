d927795922063f5d18a9981951523c605f3047ac
teoyt <125856128+teoyt@users.noreply.github.com>
2023-08-28 12:12:57 +0800

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ customhost = "database-2.cj4zpjjzzvc2.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
 custompass = "Bait3273"
 customdb = "employee"
-custombucket = "lowchoonkeat-employee1"
+custombucket = "teoyutong-bucket"
 customregion = "us-east-1"
 
